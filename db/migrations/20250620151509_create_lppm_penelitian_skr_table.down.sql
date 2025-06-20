dbad75f4cf45701bd0dbf07eda2faacdf1d76759
akbarmaulanad22 <zulfikarwxz22@gmail.com>
2025-06-20 22:23:18 +0700

    add migration penelitian

---
+++ db/migrations/20250620151509_create_lppm_penelitian_skr_table.down.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS lppm_penelitian_skr;
---
+++ db/migrations/20250620151559_create_lppm_penelitian_hpp_table.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `lppm_penelitian_hpp` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `title` varchar(30) NOT NULL,
+  `content` text NOT NULL
+) ENGINE=InnoDB
